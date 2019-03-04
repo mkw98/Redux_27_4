69f0e016507701b65798c70d94aa7883b3a0ad2c
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-04 18:42:12 +0100

    init project

---
+++ actions.js
@@ -0,0 +1,50 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';	
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+	return {
+		type: ADD_COMMENT,
+		text,
+		id: uuid.v4()
+	}
+}
+
+const boundAddComment = text => dispatch(addComment(text));
+
+function removeComment(id) {
+	return {
+		type: REMOVE_COMMENT,
+		id: id
+	}
+}
+const boundRemoveComment = id => dispatch(removeComment(id));
+
+function editComment(id, text) {
+	return {
+		type: EDIT_COMMENT,
+		id: id,
+		text: text
+	}
+}
+const boundEditComment = (id, text) => dispatch(editComment(id, text));
+
+function thumbUpComment(id) {
+	return {
+		type: THUMB_UP_COMMENT,
+		id: id
+	}
+}
+const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+
+function thumbDownComment(id) {
+	return {
+		type: THUMB_DOWN_COMMENT,
+		id: id
+	}
+}
+const boundThumbDownComment = id => dispatch(thumbDownComment(id));
