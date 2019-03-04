663f11b29957dd2415696cade35c461b7264b427
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-04 20:49:45 +0100

    edit actions, coments, reducers.js

---
+++ reducers.js
@@ -0,0 +1,24 @@
+//import {ADD_COMMENT} from './actions'
+//import {REMOVE_COMMENT} from './actions'
+//import {EDIT_COMMENT} from './actions'
+//import {THUMB_UP_COMMENT} from './actions'
+//import {THUMB_DOWN_COMMENT} from './actions'
+//to juz chyba tu niepotrzebne?
+
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
+
+const initialState = {
+    comments: [],
+    users: []
+};
+
+const app = combineReducers({
+    comments,
+    users
+});
+
+
+
+
