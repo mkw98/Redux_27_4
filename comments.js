e3e81eafd21e72b350507f7035eae292a31421cb
Maria Wieckowska <mkw98@gazeta.pl>
2019-03-05 15:46:28 +0100

    fix indentation

--- comments.js
+++ comments.js
@@ -17,28 +17,26 @@ export function comments(state = [], action) {
             return: state.filter(comment => comment.id !== action.id);
         case EDIT_COMMENT:
         	return state.map(comment => {
-			  if(comment.id === action.id) {
-				return {...comment, text: action.text} ;
-			  } else {
-				return comment;
-			  }
+				if (comment.id === action.id) {
+					return {...comment, text: action.text} ;
+				} else {
+					return comment;
+				}
 		    });
         case THUMB_UP_COMMENT:
             return state.map(comment => {
                 if (comment.id === action.id) {
-                  return {...comment, votes: comment.votes + 1}
-                }
-            	else {
-				  return comment;
+                	return {...comment, votes: comment.votes + 1}
+                } else {
+					return comment;
 				}
             });
         case THUMB_DOWN_COMMENT:
             return state.map(comment => {
                 if (comment.id === action.id) {
-                  return {...comment, votes: comment.votes - 1}
-                }
-            	else {
-				  return comment;
+                	return {...comment, votes: comment.votes - 1}
+                } else {
+					return comment;
 				}
             });
         default:
