b19256eb89debfba1d652631fac950df12be0e51
mkw98 <40563214+mkw98@users.noreply.github.com>
2019-03-05 15:37:07 +0100

    remove redundant import

--- reducers.js
+++ reducers.js
@@ -1,13 +1,5 @@
-//import {ADD_COMMENT} from './actions'
-//import {REMOVE_COMMENT} from './actions'
-//import {EDIT_COMMENT} from './actions'
-//import {THUMB_UP_COMMENT} from './actions'
-//import {THUMB_DOWN_COMMENT} from './actions'
-//to juz chyba tu niepotrzebne?
-
 import { combineReducers } from 'redux';
 import comments from './comments';
-import users from './users';
 
 const initialState = {
     comments: [],
